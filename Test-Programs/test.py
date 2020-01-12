fa5bf0b4510dc4e908b413e9ffc4ec73e8ea15de
abhilashmaipady <abhilashmaipady@gmail.com>
2020-01-13 01:51:58 +0530

    test

--- Test-Programs/test.py
+++ Test-Programs/test.py
@@ -1,6 +1,41 @@
-#this is test program
+# First Tensorflow program
 import tensorflow as tf
-sess = tf.Session()
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-hello = tf.constant("Hello world")
-print(sess.run(hello))
+num_house = 200
+np.random.seed(42)
+house_size = np.random.randint(low=1000, high=3500, size=num_house)
+
+np.random.seed(42)
+house_price = house_size * 250.0 + np.random.randint(low=10000, high=100000, size=num_house)
+
+plt.plot(house_size, house_price, "bx")
+plt.ylabel("Price")
+plt.xlabel("Size")
+plt.show()
+
+def normalize(array):
+    return (array - array.mean()) / array.std()
+
+num_train_samples = math.floor(num_house * 0.7)
+
+train_house_size = np.asarray(house_size[:num_train_samples])
+train_price = np.asarray(house_price[:num_train_samples:])
+
+train_house_size_norm = normalize(train_house_size)
+train_price_norm = normalize(train_price)
+
+
+test_house_size = np.array(house_size[num_train_samples:])
+test_house_price = np.array(house_price[num_train_samples:])
+
+test_house_size_norm = normalize(test_house_size)
+test_house_price_norm = normalize(test_house_price)
+
+# sess = tf.Session()
+
+# hello = tf.constant("Hello world")
+# print(sess.run(hello))
