86c181a20361745614adcad600508d13a76131d5
Abhilash L <abhilashmaipady@gmail.com>
2020-02-08 12:56:28 +0530

    Adding python script

--- Test-Programs/test.py
+++ Test-Programs/test.py
@@ -1,41 +1,11 @@
-# First Tensorflow program
-import tensorflow as tf
+import tensorflow as ts
 import numpy as np
-import math
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from tensorflow import keras
 
-num_house = 200
-np.random.seed(42)
-house_size = np.random.randint(low=1000, high=3500, size=num_house)
+model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+xs = np.array([-1.0,0.0,1.0,2.0,3.0,4.0], dtype=float)
+ys = np.array([-3.0,-1.0,1.0,3.0,5.0,7.0], dtype=float)
 
-np.random.seed(42)
-house_price = house_size * 250.0 + np.random.randint(low=10000, high=100000, size=num_house)
-
-plt.plot(house_size, house_price, "bx")
-plt.ylabel("Price")
-plt.xlabel("Size")
-plt.show()
-
-def normalize(array):
-    return (array - array.mean()) / array.std()
-
-num_train_samples = math.floor(num_house * 0.7)
-
-train_house_size = np.asarray(house_size[:num_train_samples])
-train_price = np.asarray(house_price[:num_train_samples:])
-
-train_house_size_norm = normalize(train_house_size)
-train_price_norm = normalize(train_price)
-
-
-test_house_size = np.array(house_size[num_train_samples:])
-test_house_price = np.array(house_price[num_train_samples:])
-
-test_house_size_norm = normalize(test_house_size)
-test_house_price_norm = normalize(test_house_price)
-
-# sess = tf.Session()
-
-# hello = tf.constant("Hello world")
-# print(sess.run(hello))
+model.fit(xs, ys, epochs=1000)
+print(model.predict([10.0]))
---
+++ identify-shoe.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow import keras
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_label), (test_images, test_labels) = fashion_mnist.load_data()
+model = keras.Sequential([
+                          keras.layers.Flatten(input_shape=(28,28)),
+                          keras.layers.Dense(128, activation=tf.nn.relu),
+                          keras.layers.Dense(10, activation=tf.nn.softmax)
+                          ])
+model.compile(optimizer=tf.train.AdamOptimizer(), loss='sparse_categorical_crossentropy')
+model.fit(train_images, train_label, epochs=100)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
